2452b6d6f84e6e7fe7b722bc76f5d1f8e4ae6bd5
LuisBerna <luis_berna@ciencias.unam.mx>
2025-01-30 23:20:20 -0600

    first commit

---
+++ src/main/kotlin/com/unam/fciencias/bern_demo_api/BernDemoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.unam.fciencias.bern_demo_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BernDemoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BernDemoApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/unam/fciencias/bern_demo_api/BernDemoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unam.fciencias.bern_demo_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BernDemoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
