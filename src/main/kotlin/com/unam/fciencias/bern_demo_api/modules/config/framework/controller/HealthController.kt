cf2f354de32079a1d9618004fbe0454d55b100e0
LuisBerna <luis_berna@ciencias.unam.mx>
2025-02-09 14:18:34 -0600

    chore: move healt controller to module

--- src/main/kotlin/com/unam/fciencias/bern_demo_api/modules/config/framework/controller/HealthController.kt
+++ src/main/kotlin/com/unam/fciencias/bern_demo_api/modules/config/framework/controller/HealthController.kt
@@ -1,4 +1,4 @@
-package com.unam.fciencias.bern_demo_api
+package com.unam.fciencias.bern_demo_api.modules.config.framework.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
