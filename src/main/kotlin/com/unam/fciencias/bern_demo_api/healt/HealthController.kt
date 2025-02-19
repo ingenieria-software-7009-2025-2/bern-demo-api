b129729489106684b28515e18657858d8c01eb42
LuisBerna <luis_berna@ciencias.unam.mx>
2025-02-18 22:24:10 -0600

    chore: se agrega ejemplo de mascota

--- src/main/kotlin/com/unam/fciencias/bern_demo_api/healt/HealthController.kt
+++ src/main/kotlin/com/unam/fciencias/bern_demo_api/healt/HealthController.kt
@@ -1,4 +1,4 @@
-package com.unam.fciencias.bern_demo_api.modules.config.framework.controller
+package com.unam.fciencias.bern_demo_api.mascota
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
