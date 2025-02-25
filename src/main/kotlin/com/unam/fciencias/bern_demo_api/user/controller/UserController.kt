929dc42b10d9b7934ba73c3a1f62e2105716409c
LuisBerna <luis_berna@ciencias.unam.mx>
2025-02-25 01:03:08 -0600

    feat: add user to DB

--- src/main/kotlin/com/unam/fciencias/bern_demo_api/user/controller/UserController.kt
+++ src/main/kotlin/com/unam/fciencias/bern_demo_api/user/controller/UserController.kt
@@ -4,6 +4,7 @@ import com.unam.fciencias.bern_demo_api.user.controller.body.LoginUserBody
 import com.unam.fciencias.bern_demo_api.user.controller.body.UserBody
 import com.unam.fciencias.bern_demo_api.user.repository.UserRepository
 import com.unam.fciencias.bern_demo_api.user.domain.Usuario
+import com.unam.fciencias.bern_demo_api.user.repository.entity.User
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,10 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/v1/users")
 class UserController(var userRepository: UserRepository) {
 
-    @PostMapping()
-    fun addUser(@RequestBody userBody: UserBody): ResponseEntity<Usuario> {
-        val usuario = Usuario(nombre = userBody.nombre, correo = userBody.mail, password = userBody.password)
-        return ResponseEntity.ok(usuario)
+    @PostMapping
+    fun addUser(@RequestBody userBody: UserBody): ResponseEntity<Any> {
+        //Convertir los datos del request a un objeto del dominio
+        val usuario = Usuario(nombre = userBody.nombre, password = userBody.password, correo = userBody.mail)
+
+
+        //Convertir el objeto del dominio hacia un objeto de nuestra BD
+        val usuarioDB =
+            User(name = usuario.nombre, password = usuario.password!!, mail = usuario.correo, token = usuario.token)
+
+        val result = userRepository.save(usuarioDB)
+        return ResponseEntity.ok(result)
+
     }
 
     @PostMapping("/login")
@@ -39,23 +49,9 @@ class UserController(var userRepository: UserRepository) {
         return ResponseEntity.ok(userFake)
     }
 
-    /*@GetMapping
+    @GetMapping
     fun getAllUsers(): ResponseEntity<Any> {
         val result = userRepository.findAll()
         return ResponseEntity.ok(result)
     }
-
-    @PostMapping
-    fun createUser(@RequestBody userPayload: UserPayload): ResponseEntity<Any> {
-        //Convertir los datos del request a un objeto del dominio
-        val usuario = Usuario(nombre = userPayload.nombre, password = userPayload.password, correo = userPayload.mail)
-
-
-        //Convertir el objeto del dominio hacia un objeto de nuestra BD
-        val usuarioDB = User(name = usuario.nombre, password = usuario.password!!, mail = usuario.correo, token = usuario.token)
-
-        val result = userRepository.save(usuarioDB)
-        return ResponseEntity.ok(result)
-    
-    }*/
 }
