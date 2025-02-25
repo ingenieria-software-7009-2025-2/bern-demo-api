e75262769cb09580ed275afef5cf94997187f5b5
LuisBerna <luis_berna@ciencias.unam.mx>
2025-02-25 00:21:23 -0600

    chore: add query custom

--- src/main/kotlin/com/unam/fciencias/bern_demo_api/user/repository/UserRepository.kt
+++ src/main/kotlin/com/unam/fciencias/bern_demo_api/user/repository/UserRepository.kt
@@ -1,7 +1,11 @@
 package com.unam.fciencias.bern_demo_api.user.repository
 
 import com.unam.fciencias.bern_demo_api.user.repository.entity.User
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository : CrudRepository<User, Int> {
+
+    @Query(value = "SELECT * FROM usuario WHERE correo=?1", nativeQuery = true)
+    fun findByEmail(email: String): User
 }
